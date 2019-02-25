a6b75db02646c2aa644b1f9667e2902d61920fa4
Rick Brown <rickbr0wn@ricks-mbp.home>
2019-02-25 22:56:43 +0000

    Setup & working

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+const Title = ({ title }) => <div>{title}</div>
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'>
+            <h1>Welcome To REACT</h1>
+            <Title title={'This is the title prop'} />
           </a>
+          <ul className='test-class'>
+            <li>1</li>
+            <li>1</li>
+            <li>1</li>
+          </ul>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+configure({ adapter: new Adapter() })
+
+describe('<App/>', () => {
+  it('should contain only 1 <p> element', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('p').length).toBe(1)
+  })
+})
+
+describe('<App/>', () => {
+  it('should contain an element with the className of "App-link"', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('.App-link').exists()).toBe(true)
+  })
+})
+
+describe('<App/>', () => {
+  it('should contain a <ul> element containing 3 <li> tags', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('ul').children().length).toBe(3)
+  })
+})
+
+describe('<App/>', () => {
+  it('should contain a <ul> element with the className of "test-class"', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('ul').hasClass('test-class')).toBe(true)
+  })
+})
+
+describe('<App/>', () => {
+  it('should contain an <h1> element, with the text "Welcome To REACT"', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('h1').text()).toBe('Welcome To REACT')
+  })
+})
+
+describe('<App/>', () => {
+  it('should contain an <h1> element, with the text "Welcome To REACT"', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('[text="This is the title prop"]').text()).toBe(
+      'This is the title prop'
+    )
+  })
+})
