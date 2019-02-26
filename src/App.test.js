581859c80c90d95ade8b933bccda042639865e55
Rick Brown <rickbr0wn@ricks-mbp.home>
2019-02-26 18:58:27 +0000

    Updated more tests <App/> only

--- src/App.test.js
+++ src/App.test.js
@@ -11,41 +11,28 @@ describe('<App/>', () => {
     const wrapper = shallow(<App />)
     expect(wrapper.find('p').length).toBe(1)
   })
-})
-
-describe('<App/>', () => {
   it('should contain an element with the className of "App-link"', () => {
     const wrapper = shallow(<App />)
     expect(wrapper.find('.App-link').exists()).toBe(true)
   })
-})
-
-describe('<App/>', () => {
   it('should contain a <ul> element containing 3 <li> tags', () => {
     const wrapper = shallow(<App />)
     expect(wrapper.find('ul').children().length).toBe(3)
   })
-})
-
-describe('<App/>', () => {
   it('should contain a <ul> element with the className of "test-class"', () => {
     const wrapper = shallow(<App />)
     expect(wrapper.find('ul').hasClass('test-class')).toBe(true)
   })
-})
-
-describe('<App/>', () => {
   it('should contain an <h1> element, with the text "Welcome To REACT"', () => {
     const wrapper = shallow(<App />)
     expect(wrapper.find('h1').text()).toBe('Welcome To REACT')
   })
-})
-
-describe('<App/>', () => {
-  it('should contain an <h1> element, with the text "Welcome To REACT"', () => {
+  it('should contain an <Title/> Component, with the text "This is the title prop"', () => {
     const wrapper = shallow(<App />)
-    expect(wrapper.find('[text="This is the title prop"]').text()).toBe(
-      'This is the title prop'
-    )
+    expect(wrapper.find('Title').text()).toBe('<Title />')
+  })
+  it('matches the snapshot', () => {
+    const tree = shallow(<App />)
+    expect(tree).toMatchSnapshot()
   })
 })
